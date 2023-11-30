e3adccb8a91f6b356fa58b59ddd09e30a040d8fb
yemma203 <106160039+yemma203@users.noreply.github.com>
2023-11-30 11:17:23 +0100

    mise en place des structures de classe

--- back.php
+++ back.php
@@ -3,13 +3,12 @@
 require_once("./config.php");
 
 try {
-    $host = DB_HOST;
-    $port = DB_PORT;
-    $user = DB_USER;
-    $pwd = DB_PWD;
-    $db_name = "gestion_recette";
+    $host = DBHOST;
+    $user = DBUSER;
+    $pwd = DBPWD;
+    $db_name = DBNAME;
 
-    $connexion = new PDO("mysql:host=$host;port=$port;dbname=$db_name", $user, $pwd);
+    $connexion = new PDO("mysql:host=$host;dbname=$db_name", $user, $pwd);
     $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
     echo "Erreur lors de la connexion à la database : " . $e->getMessage();
