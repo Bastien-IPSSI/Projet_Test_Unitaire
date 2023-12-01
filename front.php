dbf4df8b8777d96e5d0c81f61432add45a63bc72
yemma203 <106160039+yemma203@users.noreply.github.com>
2023-12-01 10:15:24 +0100

    Correction des foctionnalitées d'ajout des recettes

--- front.php
+++ front.php
@@ -76,14 +76,18 @@
         if (isset($_POST["ajouter_recette"])) {
             $lst_ingredients = [];
             array_push($lst_ingredients, ["nom_ingredient" => $_POST["nom_ingredient"], "quantite" => $_POST["quantite"]]);
+            $categorie = new Categorie($_POST["categorie"]);
+            $nom_categorie = $categorie->getCategorie();
+            $categorieDAO = new CategorieDAO($connexion);
             $recettesDAO->ajouter_recette(
                 $_POST["nomRecette"],
                 $_POST["instructions"],
                 $_POST["tmp_prep"],
-                $_POST["categorie"],
+                $categorieDAO->getIdCategorie($nom_categorie),
                 $lst_ingredients,
                 $ingredientsDAO,
             );
+            exit();
         }
         ?>
 
